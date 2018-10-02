2043e701537b54631ca127b66be966e5e322b761
Zeuxis Lo <seekstudio@gmail.com>
2018-10-02 16:18:28 +0800

    improve the checking upload file logic to check the storage directory first before read the upload file

--- routes/convert.go
+++ routes/convert.go
@@ -15,19 +15,20 @@ func ConvertPost(c *gin.Context) {
     ok      := false
     message := ""
 
-    file, err := c.FormFile("file")
-    if err != nil {
-        message = "Cannot read the file from form data"
+    if utils.CreateOrIsDirectoryExists(setting.AttachmentPath) == false {
+        message = "Cannot create storage directory"
     }else{
-        fmt.Println(file.Filename)
+        file, err := c.FormFile("file")
 
-        if utils.IsTorrentFile(file.Filename) == false {
+        if err != nil {
+            message = "Cannot read the file from form data"
+        }else if utils.IsTorrentFile(file.Filename) == false {
             message = "The upload file is not torrent file"
         }else if utils.IsBiggerFile(file.Size, setting.AttachmentMaxSize) == true {
             message = fmt.Sprintf("The upload file is bigger than %dMB", setting.AttachmentMaxSize)
-        }else if utils.CreateOrIsDirectoryExists(setting.AttachmentPath) == false {
-            message = "Cannot create storage directory"
         }else{
+            // TODO: move and convert file
+
             ok      = true
             message = "Successfully, File converted"
         }
