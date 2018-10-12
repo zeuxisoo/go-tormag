47a61bab67513f9ce1a68b5a074d21f1fa219566
Zeuxis Lo <seekstudio@gmail.com>
2018-10-12 16:48:58 +0800

    add transition effect on converted result area

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -18,39 +18,45 @@
 
         <hr>
 
-        <div class="card card-default" v-if="convertedFiles.length <= 0">
-            <div class="card-header">Converted Result</div>
-            <div class="card-body">
-                <div class="alert alert-info text-center" role="alert">
-                    Please drop the files to the drop zone first
-                </div>
-            </div>
-        </div>
-
-        <div class="card card-default mb-3" v-for="convertedFile in convertedFiles" v-bind:key="convertedFile.data.id">
-            <div class="card-header font-weight-bold">{{ convertedFile.data.file }}</div>
-            <div class="card-body p-0">
-                <div class="row m-0">
-                    <div class="col-lg-2 p-1 mt-1 mb-1 font-weight-bold text-center bg-info text-white">ID</div>
-                    <div class="col-lg-10 p-1">{{ convertedFile.data.id }}</div>
-                </div>
-                <div class="row m-0">
-                    <div class="col-lg-2 p-1 mb-1 font-weight-bold text-center bg-info text-white">MD5</div>
-                    <div class="col-lg-10 p-1">{{ convertedFile.data.md5 }}</div>
-                </div>
-                <div class="row m-0">
-                    <div class="col-lg-2 p-1 mb-1 font-weight-bold text-center bg-info text-white">Magnet</div>
-                    <div class="col-lg-10 p-1">
-                        <textarea class="form-control">{{ convertedFile.data.magnet }}</textarea>
+        <transition enter-active-class="animated fadeInDown" leave-active-class="animated fadeOutUp" mode="out-in">
+            <div class="card card-default" v-if="convertedFiles.length <= 0" key="converted-files-empty">
+                <div class="card-header">Converted Result</div>
+                <div class="card-body">
+                    <div class="alert alert-info text-center" role="alert">
+                        Please drop the files to the drop zone first
                     </div>
                 </div>
             </div>
-        </div>
+            <div v-else key="converted-files-results">
+                <transition-group enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown" tag="div">
+                    <div class="card card-default mb-3" v-for="convertedFile in convertedFiles" v-bind:key="convertedFile.data.id">
+                        <div class="card-header font-weight-bold">{{ convertedFile.data.file }}</div>
+                        <div class="card-body p-0">
+                            <div class="row m-0">
+                                <div class="col-lg-2 p-1 mt-1 mb-1 font-weight-bold text-center bg-info text-white">ID</div>
+                                <div class="col-lg-10 p-1">{{ convertedFile.data.id }}</div>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-lg-2 p-1 mb-1 font-weight-bold text-center bg-info text-white">MD5</div>
+                                <div class="col-lg-10 p-1">{{ convertedFile.data.md5 }}</div>
+                            </div>
+                            <div class="row m-0">
+                                <div class="col-lg-2 p-1 mb-1 font-weight-bold text-center bg-info text-white">Magnet</div>
+                                <div class="col-lg-10 p-1">
+                                    <textarea class="form-control" v-bind:value="convertedFile.data.magnet"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </transition-group>
+            </div>
+        </transition>
     </div>
 </template>
 
 <style lang="scss">
 @import "~filepond/dist/filepond.min.css";
+@import "~animate.css/animate.min.css";
 
 .filepond--panel-root {
     background-color: rgb(227, 237, 243);
@@ -98,7 +104,7 @@ export default {
                 console.log("Oops", error);
             }else{
                 // Add the converted file to converted file list
-                this.convertedFiles.push(file.serverId);
+                this.convertedFiles.unshift(file.serverId);
 
                 // Remove the completed file from drop zone
                 this.$refs.uploadZone.removeFile(file.id);
