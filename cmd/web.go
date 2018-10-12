1eaa69b9211f7c466a3811c47354dadf106e47e0
Zeuxis Lo <seekstudio@gmail.com>
2018-10-12 16:59:37 +0800

    format indent tab to space

--- cmd/web.go
+++ cmd/web.go
@@ -5,30 +5,30 @@ import (
     "time"
     "strings"
 
-	"github.com/urfave/cli"
+    "github.com/urfave/cli"
     "github.com/gin-gonic/gin"
     ginStatic "github.com/gin-contrib/static"
     "github.com/gin-contrib/cors"
-	"gopkg.in/flosch/pongo2.v3"
+    "gopkg.in/flosch/pongo2.v3"
 
     "github.com/zeuxisoo/go-tormag/pkg/setting"
     "github.com/zeuxisoo/go-tormag/pkg/render"
     "github.com/zeuxisoo/go-tormag/pkg/static"
-	"github.com/zeuxisoo/go-tormag/routes"
+    "github.com/zeuxisoo/go-tormag/routes"
 )
 
 // Web command
 var Web = cli.Command{
-	Name:        "web",
-	Usage:       "Start web interface",
-	Description: "Run web server to start the web application",
-	Action:      runWeb,
-	Flags: []cli.Flag{
-		stringFlag("address, a", "0.0.0.0", "Server address"),
-		stringFlag("port, p", "3000", "Server port"),
+    Name:        "web",
+    Usage:       "Start web interface",
+    Description: "Run web server to start the web application",
+    Action:      runWeb,
+    Flags: []cli.Flag{
+        stringFlag("address, a", "0.0.0.0", "Server address"),
+        stringFlag("port, p", "3000", "Server port"),
         stringFlag("mode, m", "dev", "Server mode"),
         stringFlag("config, c", "", "Custom server config file"),
-	},
+    },
 }
 
 func runWeb(c *cli.Context) {
@@ -53,17 +53,17 @@ func runWeb(c *cli.Context) {
         setting.Mode = c.String("mode")
     }
 
-	//
-	appURL := fmt.Sprintf("%s:%s", setting.Address, setting.Port)
+    //
+    appURL := fmt.Sprintf("%s:%s", setting.Address, setting.Port)
 
-	//
-	if strings.ToLower(setting.Mode) == "dev" {
-		gin.SetMode(gin.DebugMode)
-	}else{
-		gin.SetMode(gin.ReleaseMode)
-	}
+    //
+    if strings.ToLower(setting.Mode) == "dev" {
+        gin.SetMode(gin.DebugMode)
+    }else{
+        gin.SetMode(gin.ReleaseMode)
+    }
 
-	//
+    //
     engine := gin.Default()
     engine.MaxMultipartMemory = setting.AttachmentMaxSize << 20 // Set a lower memory limit for multipart (default: 4MB, app.ini: 8MB)
 
@@ -86,45 +86,45 @@ func runWeb(c *cli.Context) {
     engine.Use(gin.Recovery())
 
     //
-	registerRender(engine)
-	registerRoutes(engine)
+    registerRender(engine)
+    registerRoutes(engine)
 
     //
-	engine.Run(appURL)
+    engine.Run(appURL)
 }
 
 func registerRender(engine *gin.Engine) {
-	// engine.HTMLRender = render.NewBaseRender(&render.BaseOption{
-	// 	Functions: render.BaseFunctions{
-	// 		// e.g. {{ says "Hello world" ":D" }} ==> Hello world - Check - :D
-	// 		"says": func(value ...interface{}) string {
-	// 			helloWorld  := value[0].(string)
-	// 			suffixWorld := value[1].(string)
+    // engine.HTMLRender = render.NewBaseRender(&render.BaseOption{
+    // 	Functions: render.BaseFunctions{
+    // 		// e.g. {{ says "Hello world" ":D" }} ==> Hello world - Check - :D
+    // 		"says": func(value ...interface{}) string {
+    // 			helloWorld  := value[0].(string)
+    // 			suffixWorld := value[1].(string)
 
-	// 			return helloWorld + " - Check - " + suffixWorld
-	// 		},
-	// 	},
-	// })
+    // 			return helloWorld + " - Check - " + suffixWorld
+    // 		},
+    // 	},
+    // })
 
-	// engine.HTMLRender = render.NewPlushRender(&render.PlushOption{
-	// 	Functions: render.PlushFunctions{
-	//		// e.g. <%= says("Hello world", ":D") %> ==> Hello world - Check - :D
-	// 		"says": func(value string, argument string) string {
-	// 			return value + " - Check - " + argument
-	// 		},
-	// 	},
-	// })
+    // engine.HTMLRender = render.NewPlushRender(&render.PlushOption{
+    // 	Functions: render.PlushFunctions{
+    //		// e.g. <%= says("Hello world", ":D") %> ==> Hello world - Check - :D
+    // 		"says": func(value string, argument string) string {
+    // 			return value + " - Check - " + argument
+    // 		},
+    // 	},
+    // })
 
-	engine.HTMLRender = render.NewPongo2Render(&render.Pongo2Option{
-		Filters: render.Pongo2FilterFunctions{
-			"says": func(in *pongo2.Value, param *pongo2.Value) (out *pongo2.Value, err *pongo2.Error) {
-				value    := in.String()
-				argument := param.String()
+    engine.HTMLRender = render.NewPongo2Render(&render.Pongo2Option{
+        Filters: render.Pongo2FilterFunctions{
+            "says": func(in *pongo2.Value, param *pongo2.Value) (out *pongo2.Value, err *pongo2.Error) {
+                value    := in.String()
+                argument := param.String()
 
-				return pongo2.AsValue(value + " - Check - " + argument), nil
-			},
-		},
-	})
+                return pongo2.AsValue(value + " - Check - " + argument), nil
+            },
+        },
+    })
 }
 
 func registerRoutes(engine *gin.Engine) {
